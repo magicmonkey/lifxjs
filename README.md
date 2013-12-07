5faa0b0f7d318ec3fc6055e8ee5e329b1c496546
Kevin <github@magicmonkey.org>
2013-12-07 20:40:39 +0000

    Initial commit, a half-baked library and a demo app.

---
+++ README.md
@@ -0,0 +1,20 @@
+# JS library for LIFX bulbs
+
+Some experimentation with trying to interpret the network protocol for [LIFX
+bulbs](http://lifx.co).  This is very much observational, and an evolving
+investigation, so there's not very much info here and what there is is most
+likely incorrect or inaccurate.  You have been warned.
+
+This is based on my setup of 2 bulbs in a single group.  That may or may not
+affect the network protocol.
+
+Ideally, I'll end up with a nice little NodeJS library which can be used to
+build apps with.
+
+## Files
+
+There are 2 files:
+
+  * lifx.js which is the actual library
+  * cli.js which is an example CLI app using the library
+
---
+++ cli.js
@@ -0,0 +1,90 @@
+var lifx = require('./lifx');
+var async = require('async');
+var util = require('util');
+
+var bulb;
+
+async.series([
+
+	// Init the bulb
+	function(cb) {
+		lifx.discoverAndInit(function(err, _bulb) {
+			if (err) {
+				cb(err);
+			} else {
+				bulb = _bulb;
+				console.log("Found a controller bulb at " + bulb.address);
+				cb();
+			}
+		});
+	},
+
+	// Init the UI
+	function(cb) {
+
+		console.log("Keys:");
+		console.log("Press 1 to turn the lights on");
+		console.log("Press 2 to turn the lights off");
+		console.log("Press 3 to turn the lights a dim red");
+		console.log("Press 4 to turn the lights a dim purple");
+		console.log("Press 5 to turn the lights a bright white");
+		console.log("Press a to request an info update from the lights");
+
+		var stdin = process.openStdin();
+		process.stdin.setRawMode(true)
+		process.stdin.resume();
+
+		stdin.on('data', function (key) {
+			//process.stdout.write('Got key ' + util.inspect(key) + '\n');
+			switch (key[0]) {
+
+				case 0x31: // 1
+					console.log("Lights on");
+					bulb.lightsOn();
+					break;
+
+				case 0x32: // 2
+					console.log("Lights off");
+					bulb.lightsOff();
+					break;
+
+				case 0x33: // 3
+					console.log("Dim red");
+					bulb.red();
+					break;
+
+				case 0x34: // 4
+					console.log("Dim purple");
+					bulb.purple();
+					break;
+
+				case 0x35: // 5
+					console.log("Bright white");
+					bulb.brightWhite();
+					break;
+
+				case 0x61: // a
+					console.log("Requesting info...");
+					bulb.getInfo();
+					break;
+
+				case 0x03: // ctrl-c
+					console.log("Closing...");
+					bulb.close();
+					process.stdin.pause();
+					//process.exit();
+					break;
+
+			}
+		});
+
+
+	}
+
+], function(err, results) {
+	if (err) {
+		console.log("Err: " + err);
+	}
+	console.log(results);
+});
+
