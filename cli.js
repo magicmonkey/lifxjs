62f7bc721fe140bbf32f366c1d161d1bd2e48e6b
Kevin <github@magicmonkey.org>
2013-12-08 16:18:25 +0000

    Refactored the library to acknowledge that the gateway and the bulbs are different

--- cli.js
+++ cli.js
@@ -2,101 +2,84 @@ var lifx = require('./lifx');
 var async = require('async');
 var util = require('util');
 
-var bulb;
+var gw = null;
 
-async.series([
-
-	// Init the bulb
-	function(cb) {
-		lifx.discoverAndInit(function(err, _bulb) {
-			if (err) {
-				cb(err);
-			} else {
-				bulb = _bulb;
-				console.log("Found a controller bulb at " + bulb.address);
-				cb();
-			}
-		});
-	},
-
-	// Init the UI
-	function(cb) {
-
-		console.log("Keys:");
-		console.log("Press 1 to turn the lights on");
-		console.log("Press 2 to turn the lights off");
-		console.log("Press 3 to turn the lights a dim red");
-		console.log("Press 4 to turn the lights a dim purple");
-		console.log("Press 5 to turn the lights a bright white");
-		console.log("Press 6 to cycle forwards through colours");
-		console.log("Press 7 to cycle backwards through colours");
-		console.log("Press a to request an info update from the lights");
-
-		var stdin = process.openStdin();
-		process.stdin.setRawMode(true)
-		process.stdin.resume();
-
-		stdin.on('data', function (key) {
-			//process.stdout.write('Got key ' + util.inspect(key) + '\n');
-			switch (key[0]) {
-
-				case 0x31: // 1
-					console.log("Lights on");
-					bulb.lightsOn();
-					break;
-
-				case 0x32: // 2
-					console.log("Lights off");
-					bulb.lightsOff();
-					break;
-
-				case 0x33: // 3
-					console.log("Dim red");
-					bulb.red();
-					break;
-
-				case 0x34: // 4
-					console.log("Dim purple");
-					bulb.purple();
-					break;
-
-				case 0x35: // 5
-					console.log("Bright white");
-					bulb.brightWhite();
-					break;
-
-				case 0x36: // 6
-					console.log("Test 1 (colour cycle forwards)");
-					bulb.test1();
-					break;
-
-				case 0x37: // 7
-					console.log("Test 2 (colour cycle backwards)");
-					bulb.test2();
-					break;
-
-				case 0x61: // a
-					console.log("Requesting info...");
-					bulb.getInfo();
-					break;
-
-				case 0x03: // ctrl-c
-					console.log("Closing...");
-					bulb.close();
-					process.stdin.pause();
-					//process.exit();
-					break;
-
-			}
-		});
-
-
-	}
-
-], function(err, results) {
+lifx.Gateway.discoverAndInit(function(err, _gw) {
 	if (err) {
-		console.log("Err: " + err);
+		console.log("Err " + err);
+	} else {
+		console.log("gw " + util.inspect(gw));
+		gw = _gw;
 	}
-	console.log(results);
+});
+
+console.log("Keys:");
+console.log("Press 1 to turn the lights on");
+console.log("Press 2 to turn the lights off");
+console.log("Press 3 to turn the lights a dim red");
+console.log("Press 4 to turn the lights a dim purple");
+console.log("Press 5 to turn the lights a bright white");
+console.log("Press 6 to cycle forwards through colours");
+console.log("Press 7 to cycle backwards through colours");
+console.log("Press a to request an info update from the lights");
+
+var stdin = process.openStdin();
+process.stdin.setRawMode(true)
+process.stdin.resume();
+
+var cycledColour = 0;
+
+stdin.on('data', function (key) {
+	//process.stdout.write('Got key ' + util.inspect(key) + '\n');
+	switch (key[0]) {
+
+		case 0x31: // 1
+			console.log("Lights on");
+			gw.lightsOn();
+			break;
+
+		case 0x32: // 2
+			console.log("Lights off");
+			gw.lightsOff();
+			break;
+
+		case 0x33: // 3
+			console.log("Dim red");
+			gw.lightsColour(0xd49e, 0xffff, 0x028f, 0x0dac, 0x0513);
+			break;
+
+		case 0x34: // 4
+			console.log("Dim purple");
+			gw.lightsColour(0xcc15, 0xffff, 0x028f, 0x0dac, 0x0513);
+			break;
+
+		case 0x35: // 5
+			console.log("Bright white");
+			gw.lightsColour(0x0000, 0x0000, 0x8000, 0x0af0, 0x0513);
+			break;
+
+		case 0x36: // 6
+			cycledColour = (cycledColour+100) & 0xffff; console.log("Colour value is " + cycledColour);
+			gw.lightsColour(cycledColour, 0xffff, 0x0200, 0x0000, 0x0000);
+			break;
+
+		case 0x37: // 7
+			cycledColour = (cycledColour+1000) & 0xffff; console.log("Colour value is " + cycledColour);
+			gw.lightsColour(cycledColour, 0xffff, 0x0200, 0x0000, 0x0000);
+			break;
+
+		case 0x61: // a
+			console.log("Requesting info...");
+			gw.findBulbs();
+			break;
+
+		case 0x03: // ctrl-c
+			console.log("Closing...");
+			gw.close();
+			process.stdin.pause();
+			//process.exit();
+			break;
+
+	}
 });
 
--- lifx.js
+++ lifx.js
@@ -2,97 +2,48 @@ var dgram = require('dgram');
 var net = require('net');
 var util = require('util');
 
-var UDPClient = dgram.createSocket("udp4");
 var port = 56700;
 var found = false;
 
-module.exports = {
 
-	discover: function(cb) {
+// This represents each individual bulb, and the operations on that bulb
+function Bulb(_lifxAddress) {
+	this.lifxAddress = _lifxAddress;
+}
 
-		UDPClient.on("error", function (err) {
-			console.log("UDP error " + err);
-		});
+// This represents the gateway, and its respective functions (eg discovery, send-to-all etc)
+function Gateway(addr) {
+	// TODO: validation...
+	this.bulbs = {};
+	this.ipAddress = {ip:addr.address, port:addr.port, family:addr.family};
+	this.lifxAddress = addr.gwBulb;
+	this.tcpClient = null;
+}
 
-		UDPClient.on("message", function (msg, rinfo) {
-
-			console.log(" U- " + msg.toString("hex"));
-			var gwBulb = msg.slice(16, 24);
-			if (msg.length > 4 && msg[3] == 0x54 && msg[32] == 0x03) {
-				if (!found) {
-					found = true;
-					cb(null, {address:rinfo.address, port:rinfo.port, family:rinfo.family, gwBulb:gwBulb} );
-				}
-			}
-		});
-
-		UDPClient.bind(port, function() {
-			UDPClient.setBroadcast(true);
-			var intervalID;
-			// Now send the discovery packets
-			intervalID = setInterval(function() {
-				if (found) {
-					clearInterval(intervalID);
-				} else {
-					var message = new Buffer([0x24, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00]);
-					console.log(" U+ " + message.toString("hex"));
-					UDPClient.send(message, 0, message.length, port, "255.255.255.255", function(err, bytes) {
-						if (err) {
-							cb(err);
-						}
-					});
-				}
-			}, 300);
-		});
-
-	},
-
-	init: function(addr, cb) {
-		var b = new bulb(addr);
-		cb(null, b);
-	},
-
-	discoverAndInit: function(cb) {
-		module.exports.discover(function(err, addr) {
-			if (err) {
-				cb(err);
-			} else {
-				module.exports.init(addr, cb);
-			}
-		});
-	},
-
+Gateway.prototype.addDiscoveredBulb = function(lifxAddress, bulbName) {
+	var strAddress = lifxAddress.toString('hex');
+	if (typeof this.bulbs[strAddress] == 'undefined') {
+		// It's a new bulb
+		console.log("*** NEW BULB (" + bulbName + ") ***");
+		this.bulbs[strAddress] = new Bulb(lifxAddress);
+	}
 };
 
-function bulb(addr) {
-
-	this.address = null; // IP address of the gateway bulb
-	this.port    = null; // IP port of the gateway bulb
-	this.gwBulb  = null; // LIFX address of the gateway bulb
-	var client   = null;
-
-	if (typeof addr == 'object' && typeof addr.address == 'string' && typeof addr.port == 'number') {
-		this.address = addr.address;
-		this.port = addr.port;
-		this.gwBulb = addr.gwBulb;
-	}
-
-	var self=this;
-	function connect() {
-		client = net.connect(self.port, self.address, function() { //'connect' listener
-			//console.log('client connected');
-		});
-	}
-
-	connect();
-
-	client.on('data', function(data) {
+Gateway.prototype.connect = function(cb) {
+	var self = this;
+	this.tcpClient = net.connect(this.ipAddress.port, this.ipAddress.ip, function() { //'connect' listener
+		cb();
+	});
+	this.tcpClient.on('data', function(data) {
 		console.log(" T- " + data.toString("hex"));
 		
 		switch (data[32]) {
 
 			case 0x6b:
-				console.log(" * Found a bulb: " + data.slice(48).toString("ascii"));
+				var lifxAddress = data.slice(8,16);
+				var bulbName = data.slice(48);
+				console.log(" * Found a bulb: " + bulbName + " (address " + util.inspect(lifxAddress) + ")");
+				self.addDiscoveredBulb(lifxAddress, bulbName);
 				break;
 
 			case 0x16:
@@ -104,27 +55,123 @@ function bulb(addr) {
 				break;
 		}
 	});
-	client.on('end', function() {
+	this.tcpClient.on('end', function() {
 		console.log('TCP client disconnected');
-		//connect();
 	});
+}
 
-	var self = this;
+Gateway.prototype.findBulbs = function(cb) {
+	this.sendToAll(new Buffer([0x65, 0x00, 0x00, 0x00]));
+};
 
+Gateway.prototype.sendToAll = function(message) {
 	var standardPrefix = new Buffer([0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
 	// Splice in the address of our discovered gateway bulb to replace these bytes --------------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-	this.gwBulb.copy(standardPrefix, 15);
+	this.lifxAddress.copy(standardPrefix, 15);
 
-	this.sendRawPacket = function(message) {
-		var bLen = new Buffer([message.length + standardPrefix.length + 1]);
-		var sendBuf = Buffer.concat([
-			bLen,
-			standardPrefix,
-			message
-		]);
-		console.log(" T+ " + sendBuf.toString("hex"));
-		client.write(sendBuf);
-	};
+	var bLen = new Buffer([message.length + standardPrefix.length + 1]);
+	var sendBuf = Buffer.concat([
+		bLen,
+		standardPrefix,
+		message
+	]);
+	console.log(" T+ " + sendBuf.toString("hex"));
+	this.tcpClient.write(sendBuf);
+};
+
+Gateway.prototype.close = function() {
+	this.tcpClient.end();
+};
+
+Gateway.discoverAndInit = function(cb) {
+	Gateway.discover(function(err, addr) {
+		if (err) {
+			cb(err);
+		} else {
+			Gateway.init(addr, cb);
+		}
+	});
+};
+
+Gateway.init = function(addr, cb) {
+	var g = new Gateway(addr);
+	g.connect(function() {
+		g.findBulbs();
+	});
+	cb(null, g);
+};
+
+Gateway.discover = function(cb) {
+
+	var UDPClient = dgram.createSocket("udp4");
+	UDPClient.unref(); // Stop this from preventing Node from ending
+	
+	UDPClient.on("error", function (err) {
+		console.log("UDP error " + err);
+	});
+
+	UDPClient.on("message", function (msg, rinfo) {
+
+		console.log(" U- " + msg.toString("hex"));
+		var gwBulb = msg.slice(16, 24);
+		if (msg.length > 4 && msg[3] == 0x54 && msg[32] == 0x03) {
+			if (!found) {
+				found = true;
+				cb(null, {address:rinfo.address, port:rinfo.port, family:rinfo.family, gwBulb:gwBulb} );
+			}
+		}
+	});
+
+	UDPClient.bind(port, function() {
+		UDPClient.setBroadcast(true);
+		var intervalID;
+		// Now send the discovery packets
+		intervalID = setInterval(function() {
+			if (found) {
+				clearInterval(intervalID);
+			} else {
+				var message = new Buffer([0x24, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00]);
+				console.log(" U+ " + message.toString("hex"));
+				UDPClient.send(message, 0, message.length, port, "255.255.255.255", function(err, bytes) {
+					if (err) {
+						cb(err);
+					}
+				});
+			}
+		}, 300);
+	});
+};
+
+Gateway.prototype.lightsOn = function() {
+	this.sendToAll(new Buffer([0x15, 0x00, 0x00, 0x00, 0x01, 0x00]));
+};
+
+Gateway.prototype.lightsOff = function() {
+	this.sendToAll(new Buffer([0x15, 0x00, 0x00, 0x00, 0x00, 0x00]));
+};
+
+// Pass in 16-bit numbers for each param - they will be byte shuffled as appropriate
+Gateway.prototype.lightsColour = function(hue, sat, lum, whitecol, timing) {
+	var message = new Buffer([0x66, 0x00, 0x00, 0x00, 0x00, 0x9e, 0xd4, 0xff, 0xff, 0x8f, 0x02, 0xac, 0x0d, 0x13, 0x05, 0x00, 0x00]);
+	//                                                      ^^^^^^^^^^  ^^^^^^^^^^  ^^^^^^^^^^  ^^^^^^^^^^  ^^^^^^^^^^
+	//                                                         hue      saturation  luminance  white colour   timing
+	message[5]  = (hue & 0x00ff);
+	message[6]  = (hue & 0xff00) >> 8;
+	message[7]  = (sat & 0x00ff);
+	message[8]  = (sat & 0xff00) >> 8;
+	message[9]  = (lum & 0x00ff);
+	message[10] = (lum & 0xff00) >> 8;
+	message[11] = (whitecol & 0x00ff);
+	message[12] = (whitecol & 0xff00) >> 8;
+	message[13] = (timing & 0x00ff);
+	message[14] = (timing & 0xff00) >> 8;
+
+	this.sendToAll(message);
+};
+
+function bulbz(addr) {
+
+	var self = this;
 
 	var i = 0;
 	this.test1 = function() {
@@ -141,54 +188,9 @@ function bulb(addr) {
 		//                                                              hue      saturation  luminance   white colour  timing
 	};
 
-	// Pass in 16-bit numbers for each param - they will be byte shuffled as appropriate
-	this.lightsColour = function(hue, sat, lum, timing) {
-		var message = new Buffer([0x66, 0x00, 0x00, 0x00, 0x00, 0x9e, 0xd4, 0xff, 0xff, 0x8f, 0x02, 0xac, 0x0d, 0x13, 0x05, 0x00, 0x00]);
-		//                                                      ^^^^^^^^^^  ^^^^^^^^^^  ^^^^^^^^^^              ^^^^^^^^^^
-		//                                                         hue      saturation  luminance                 timing
-		message[5]  = (hue & 0x00ff);
-		message[6]  = (hue & 0xff00) >> 8;
-		message[7]  = (sat & 0x00ff);
-		message[8]  = (sat & 0xff00) >> 8;
-		message[9]  = (lum & 0x00ff);
-		message[10] = (lum & 0xff00) >> 8;
-		message[13] = (timing & 0x00ff);
-		message[14] = (timing & 0xff00) >> 8;
-
-		self.sendRawPacket(message);
-		
-	};
-
-	this.lightsOn = function() {
-		self.sendRawPacket(new Buffer([0x15, 0x00, 0x00, 0x00, 0x01, 0x00]));
-	};
-
-	this.lightsOff = function() {
-		self.sendRawPacket(new Buffer([0x15, 0x00, 0x00, 0x00, 0x00, 0x00]));
-	};
-
-	this.red = function() {
-		self.lightsColour(0xd49e, 0xffff, 0x028f, 0x0513);
-	};
-
-	this.purple = function() {
-		self.lightsColour(0xcc15, 0xffff, 0x028f, 0x0513);
-	};
-
-	this.brightWhite = function() {
-		self.sendRawPacket(new Buffer([0x66, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x83, 0x96, 0xf0, 0x0a, 0x90, 0x01, 0x00, 0x00]));
-		//                                                           ^^^^^^^^^^  ^^^^^^^^^^  ^^^^^^^^^^              ^^^^^^^^^^
-		//                                                              hue      saturation  luminance                 timing
-	};
-
-	this.getInfo = function() {
-		self.sendRawPacket(new Buffer([0x65, 0x00, 0x00, 0x00]));
-	};
-
-	this.close = function() {
-		UDPClient.close();
-		client.end();
-	};
-
 }
+
+module.exports = {
+	Gateway:Gateway
+};
 
