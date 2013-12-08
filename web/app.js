c84abcb8d21f26c327ed50b32adaec8aeb738755
Kevin <github@magicmonkey.org>
2013-12-08 23:22:26 +0000

    Friendlier URL

--- web/app.js
+++ web/app.js
@@ -2,7 +2,7 @@ var util = require('util');
 var express = require('express');
 
 var app = express();
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/', express.static(__dirname + '/static'));
 
 app.get('/set/bulb1', function(req, res, next) {
 	var bulb1 = req.query.bulb1;
@@ -10,7 +10,7 @@ app.get('/set/bulb1', function(req, res, next) {
 	var hue = bulb1.h * (65536 / 360)
 	var sat = bulb1.s * (65536 / 0.8)
 	var lum = bulb1.l * (65536 / 1)
-	console.log(hue, sat, lum);
+	//console.log(hue, sat, lum);
 	gw.lightsColour(hue, sat, lum, 0x0dac, 0);
 	res.end("OK");
 });
