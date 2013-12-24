14e3441b2dd378d707421eca39c9c059553ee569
Kevin <github@magicmonkey.org>
2013-12-24 16:27:24 +0000

    Refactored to use the LE numbers everywhere and sorted out some of the trees and column labels

--- wireshark/lifx.lua
+++ wireshark/lifx.lua
@@ -1,4 +1,18 @@
 local lifx = Proto("lifx", "LIFX wifi lightbulb")
+local F = lifx.fields
+
+function switch(t)
+	t.case = function (self, pType, buffer, pinfo, tree)
+		local f=self[pType]
+		if packetNames[pType] then pinfo.cols.info = packetNames[pType] end
+		if f then
+			if type(f)=="function" then
+				f(buffer, pinfo, tree, self)
+			end
+		end
+	end
+	return t
+end
 
 local onOffStrings = {
 	[0x0000] = "Off",
@@ -6,7 +20,107 @@ local onOffStrings = {
 	[0xffff] = "On"
 }
 
-local F = lifx.fields
+packetNames = {
+	[0x0002]   = "Get PAN Gateway",
+	[0x0003]   = "Device state response",
+	[0x0004]   = "Get time",
+	[0x0005]   = "Set time",
+	[0x0006]   = "Time state",
+	[0x0007]   = "Get reset switch",
+	[0x0008]   = "Reset switch state",
+	[0x0009]   = "Get dummy load",
+	[0x000a]   = "Set dummy load",
+	[0x000b]   = "Dummy load",
+	[0x000c]   = "Get mesh info",
+	[0x000d]   = "Mesh info",
+	[0x000e]   = "Get mesh firmware",
+	[0x000f]   = "Mesh firmware state",
+	[0x0010]   = "Get wifi info",
+	[0x0011]   = "Wifi info",
+	[0x0012]   = "Get wifi firmware state",
+	[0x0013]   = "Wifi firmware state",
+	[0x0014]   = "Get power state",
+	[0x0015]   = "Set power state",
+	[0x0016]   = "Power state",
+	[0x0017]   = "Get bulb label",
+	[0x0018]   = "Set bulb label",
+	[0x0019]   = "Bulb label",
+	[0x001a]   = "Get tags",
+	[0x001c]   = "Tags",
+	[0x001d]   = "Get tag labels",
+	[0x001f]   = "Tag labels",
+	[0x0020]   = "Get version",
+	[0x0022]   = "Get info",
+	[0x0023]   = "Info",
+	[0x0024]   = "Get MCU rail voltage",
+	[0x0025]   = "MCU rail voltage",
+	[0x0026]   = "Reboot",
+	[0x0065]   = "Get light state",
+	[0x0066]   = "Set light color",
+	[0x0067]   = "Set waveform",
+	[0x0068]   = "Set dim (absolute)",
+	[0x0069]   = "Set dim (relative)",
+	[0x006b]   = "Light status",
+	[0x012d]   = "Get wifi state",
+	[0x012e]   = "Set wifi state",
+	[0x012f]   = "Wifi state",
+	[0x0130]   = "Get access points",
+	[0x0131]   = "Set access point",
+	[0x0132]   = "Access point"
+}
+
+function unknownPacket(buffer, pinfo, tree)
+	print("Unknown packet "..buffer(0,2):uint().." in packet number "..pinfo.number)
+end
+
+function deviceState(buffer, pinfo, tree)
+	tree:add(F.unknown1, buffer(0, 1))
+	tree:add(F.unknown2, buffer(1, 4))
+end
+
+function onoffRequest(buffer, pinfo, tree)
+	tree:add(F.onoffReq, buffer(0, 1))
+end
+
+function changeName(buffer, pinfo, tree)
+	tree:add(F.bulbName, buffer(0))
+end
+
+function setBulbState(buffer, pinfo, tree)
+	tree:add_le(F.hue, buffer(1,2))
+	tree:add_le(F.saturation, buffer(3,2))
+	tree:add_le(F.luminance,  buffer(5,2))
+	tree:add_le(F.colourTemp, buffer(7,2))
+	tree:add_le(F.fadeTime,   buffer(8,2))
+end
+
+function onoffResponse(buffer, pinfo, tree)
+	tree:add(F.onoffRes, buffer(0, 2))
+end
+
+function changeNameResponse(buffer, pinfo, tree)
+	tree:add(F.bulbName, buffer(0))
+end
+
+function statusResponse(buffer, pinfo, tree)
+	tree:add_le(F.hue       , buffer(0 , 2))
+	tree:add_le(F.saturation, buffer(2 , 2))
+	tree:add_le(F.luminance , buffer(4 , 2))
+	tree:add_le(F.colourTemp, buffer(6 , 2))
+	tree:add(F.reserved     , buffer(8 , 2))
+	tree:add(F.onoffRes     , buffer(10, 2))
+	tree:add(F.bulbName     , buffer(12))
+end
+
+packetTable = switch {
+	[0x0003] = deviceState,
+	[0x0015] = onoffRequest,
+	[0x0018] = changeName,
+	[0x0066] = setBulbState,
+	[0x0016] = onoffResponse,
+	[0x0019] = changeNameResponse,
+	[0x006b] = statusResponse
+}
 
 F.size       = ProtoField.uint16("lifx.size"         , "Packet size"                 , base.HEX)
 F.protocol   = ProtoField.uint16("lifx.protocol"     , "LIFX protocol"               , base.HEX)
@@ -14,7 +128,7 @@ F.reserved   = ProtoField.bytes("lifx.reserved"      , "Reserved"               
 F.targetAddr = ProtoField.ether("lifx.targetAddr"    , "Target address"              , base.HEX)
 F.site       = ProtoField.ether("lifx.site"          , "Site address"                , base.HEX)
 F.timestamp  = ProtoField.uint64("lifx.timestamp"    , "Timestamp"                   , base.HEX)
-F.packetType = ProtoField.uint16("lifx.packetType"   , "Packet type"                 , base.HEX)
+F.packetType = ProtoField.uint16("lifx.packetType"   , "Packet type"                 , base.HEX, packetNames)
 
 F.unknown1   = ProtoField.uint8("lifx.unknown1"      , "Unknown - always 1 or 2"     , base.HEX)
 F.unknown2   = ProtoField.uint32("lifx.unknown2"     , "Unknown - always 7c dd 00 00", base.HEX)
@@ -27,87 +141,6 @@ F.colourTemp = ProtoField.uint16("lifx.colourTemp"   , "Colour temperature"     
 F.fadeTime   = ProtoField.uint16("lifx.fadeTime"     , "Fade time"                   , base.HEX)
 F.onoffRes   = ProtoField.uint16("lifx.onoffResponse", "On/off"                      , base.HEX, onOffStrings)
 
-function switch(t)
-	t.case = function (self, pType, buffer, pinfo, tree)
-		local f=self[pType] or self.default
-		if f then
-			-- local subtree = tree:add(f[2])
-			tree:append_text(" ("..f[2]..")")
-			pinfo.cols.info = f[2]
-			if type(f[1])=="function" then
-				f[1](buffer, pinfo, tree, self)
-			end
-		end
-	end
-	return t
-end
-
-function unknownPacket(buffer, pinfo, tree)
-	print("Unknown packet "..buffer(0,2):uint().." in packet number "..pinfo.number)
-end
-
--- 0x03 or 768
-function deviceState(buffer, pinfo, tree)
-	tree:add(F.unknown1, buffer(0, 1))
-	tree:add(F.unknown2, buffer(1, 4))
-end
-
--- 0x15 or 5376
-function onoffRequest(buffer, pinfo, tree)
-	tree:add(F.onoffReq, buffer(0, 1))
-end
-
--- 0x18 or 6144
-function changeName(buffer, pinfo, tree)
-	tree:add(F.bulbName, buffer(0))
-end
-
--- 0x66 or 26112
-function setBulbState(buffer, pinfo, tree)
-	tree:add_le(F.hue, buffer(1,2))
-	tree:add_le(F.saturation, buffer(3,2))
-	tree:add_le(F.luminance,  buffer(5,2))
-	tree:add_le(F.colourTemp, buffer(7,2))
-	tree:add_le(F.fadeTime,   buffer(8,2))
-end
-
--- 0x16 or 5632
-function onoffResponse(buffer, pinfo, tree)
-	tree:add(F.onoffRes, buffer(0, 2))
-end
-
--- 0x19 or 6400
-function changeNameResponse(buffer, pinfo, tree)
-	tree:add(F.bulbName, buffer(0))
-end
-
--- 0x6b or 27392
-function statusResponse(buffer, pinfo, tree)
-	tree:add_le(F.hue       , buffer(0 , 2))
-	tree:add_le(F.saturation, buffer(2 , 2))
-	tree:add_le(F.luminance , buffer(4 , 2))
-	tree:add_le(F.colourTemp, buffer(6 , 2))
-	tree:add(F.reserved     , buffer(8 , 2))
-	tree:add(F.onoffRes     , buffer(10, 2))
-	tree:add(F.bulbName     , buffer(12))
-end
-
-packetTable = switch {
-	[768]   = { deviceState       , "Device state response"},
-	[512]   = { nil               , "Get PAN Gateway"},
-	[5376]  = { onoffRequest      , "On/off request"},
-	[6144]  = { changeName        , "Change name request"},
-	[25856] = { nil               , "Status request"},
-	[26112] = { setBulbState      , "Set bulb state"},
-	[5632]  = { onoffResponse     , "On/off response"},
-	[6400]  = { changeNameResponse, "Change name response"},
-	[27392] = { statusResponse    , "Status response"},
-	[26624] = { nil               , "Set dim - absolute"},
-	[26880] = { nil               , "Set dim - relative"},
-	[12033] = { nil               , "Wifi state"},
-	default = { unknownPacket     , "Unknown packet"}
-}
-
 function lifx.dissector(buffer, pinfo, tree)
 	analyse(buffer, pinfo, tree)
 end
@@ -116,7 +149,7 @@ function analyse(buffer, pinfo, tree)
 	pinfo.cols.info = "LIFX"
 
 	-- LE maths on the packet length
-	local lifxlength = buffer(0,1):uint() + (buffer(1,1):uint() * 256)
+	local lifxlength = buffer(0,2):le_uint()
 
 	local subtree = tree:add(buffer(0, lifxlength), "LIFX packet")
 	subtree:add_le(F.size, buffer(0,2))
@@ -130,8 +163,8 @@ function analyse(buffer, pinfo, tree)
 	local packetPayload = subtree:add_le(F.packetType, buffer(32,2))
 	subtree:add(F.reserved, buffer(34,2))
 
-	-- local pSubtree = subtree:add(buffer(36, lifxlength-36), "Payload")
-	packetTable:case(buffer(32,2):uint(), buffer(36), pinfo, packetPayload)
+	-- Call the packet-specific handler
+	packetTable:case(buffer(32,2):le_uint(), buffer(36), pinfo, packetPayload)
 
 	-- Check if there's another LIFX packet inside this TCP packet
 	if (lifxlength > 0 and buffer:len() > lifxlength) then
